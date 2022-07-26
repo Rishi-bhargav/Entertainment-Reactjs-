11a91ce10a19467e93511e3dbb80eeaa2db9d0f7
Rishi Bhargav <rishi.bhargav@intimetec.com>
2022-07-26 23:39:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Switch, Route,Routes, Link } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import Trending from "./Pages/Trending/Trending";
+import Movies from "./Pages/Movies/Movies";
+import Series from "./Pages/Series/Series";
+import Search from "./Pages/Search/Search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <Container>
+          <Router>
+            <Routes>
+            <Route path="/" element={Trending} />
+            <Route  path="/movies" element={Movies} />
+            <Route  path="/series" element={Series} />
+            <Route path="/search" element={Search} />
+            </Routes>
+            </Router>
+        </Container>
+      </div>
+      <Footer />
+    </>
+
   );
 }
 
