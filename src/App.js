9fb898c3d2c4c0fea18d72a6cdd5faa155aae9bd
Ankur Srivastava <ankur.srivastava@intimetec.com>
2022-07-27 14:14:00 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import "./App.css";
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Switch, Route,Routes, Link } from "react-router-dom";
-import { Container } from "@material-ui/core";
-import Trending from "./Pages/Trending/Trending";
-import Movies from "./Pages/Movies/Movies";
-import Series from "./Pages/Series/Series";
-import Search from "./Pages/Search/Search";
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import { Route, Routes } from 'react-router-dom'
+import { Container } from '@material-ui/core'
+import Trending from './Pages/Trending/Trending'
+import Movies from './Pages/Movies/Movies'
+import Series from './Pages/Series/Series'
+import Search from './Pages/Search/Search'
 
 function App() {
   return (
     <>
       <Header />
-      <div className="App">
+      <div className='App'>
         <Container>
-          <Router>
-            <Routes>
-            <Route path="/" element={Trending} />
-            <Route  path="/movies" element={Movies} />
-            <Route  path="/series" element={Series} />
-            <Route path="/search" element={Search} />
-            </Routes>
-            </Router>
+          <Routes>
+            <Route path='/' element={<Trending />} />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
         </Container>
       </div>
       <Footer />
     </>
-
-  );
+  )
 }
 
-export default App;
+export default App
